05138c796bb97fb166e9fe5953882cb17aa9b39b
David Rose <david@sxsw.com>
2016-01-22 12:51:18 -0500

    remove unused files

--- src/reducers/defaultReducer.js
+++
@@ -1,9 +0,0 @@
-import * as types from '../constants'
-const initialState = {}
-
-export default function defaultReducer (state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state
-  }
-}
