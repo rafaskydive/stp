44dae299fc1f5d6f5ae03d9ad447f29300f9a0a2
David Rose <david@sxsw.com>
2016-01-27 18:54:58 -0500

    use PouchDB local and synch to CouchDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,10 @@
 var PouchDB = require('pouchdb')
 
-var database = new PouchDB('http://localhost:5984/my-pouch-db')
+var database = new PouchDB('STP')
+
+const sync = PouchDB.sync('STP', 'http://localhost:5984/my-pouch-db', {
+  live: true,
+  retry: true
+})
 
 module.exports = database
