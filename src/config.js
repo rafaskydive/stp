271f666369d025cb3f24d45d61c3057ee55f3e70
David Rose <david@sxsw.com>
2016-01-27 13:11:39 -0500

    make HOME work for tests, too

--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+function home() {
+  return process.env['HOME'] || HOME
+}
 export default {
-  "videoFilePath": path.join(HOME, '.stp', 'videos')
+  "videoFilePath": path.join(home(), '.stp', 'videos')
 }
