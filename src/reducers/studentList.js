9258933ee3e94b5521336412b95e2411b8ae1fa9
David Rose <david@sxsw.com>
2016-02-03 12:28:35 -0500

    fixed sorting of StudentList. what a mess.
    closes #9

--- src/reducers/studentList.js
+++ src/reducers/studentList.js
@@ -13,12 +13,28 @@ export default function studentList (state=initialState, action) {
         students: [...action.payload]
       })
     case types.TOGGLE_SORT:
+      let students = state.students
+      const sortedStudents = []
       let sortBy = action.payload.sortBy
       let sortDesc = state.sortDesc ? false : true
-      let sortedStudents = state.students.sort((a, b) => {
-        if(sortDesc) { return a[sortBy] > b[sortBy] }
-        return a[sortBy] < b[sortBy]
-      })
+      let sortedNames = students.map(student => {
+        return student.name
+      }).sort()
+      let sortedDates = students.map(student => {
+        return student.last_jump_date
+      }).sort()
+      if(sortBy === "name") {
+        if (!sortDesc) { sortedNames.reverse() }
+        sortedNames.map(name => {
+          sortedStudents.push(students.find(student => { return student.name === name }))
+        })
+      }
+      if(sortBy === "last_jump_date") {
+        if (!sortDesc) { sortedDates.reverse() }
+        sortedDates.map(date => {
+          sortedStudents.push(students.find(student => { return student.last_jump_date === date }))
+        })
+      }
       return Object.assign({}, state, {
         sortBy: sortBy,
         sortDesc: sortDesc,
