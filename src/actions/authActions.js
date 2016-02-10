4c7b96100e5eb2af7f0febe52608c3db0f333704
David Rose <david@sxsw.com>
2016-02-10 17:34:02 -0500

    store users in db doc

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -2,12 +2,6 @@ import * as types from '../constants'
 import database from '../database'
 import bcrypt from 'bcrypt-nodejs'
 
-const users = {
-  "stp": {
-    hashed_password: "$2a$10$b6cgfiN7YxK3RaCIc7tWVeMfuFNDqe6z7gHhyaXegq8w03EQdwMka"
-  }
-}
-
 export function editField (target) {
   return {
     type: types.EDIT_AUTH_FIELD,
@@ -17,25 +11,33 @@ export function editField (target) {
 
 export function login (user, callback) {
   return dispatch => {
-    let foundUser = users[user.username]
-    if (! foundUser ) {
-      return dispatch({
-        type: types.AUTH_ERROR,
-        error: `User '${user.username}' Not Found`
+    database.get('users', (err, users) => {
+      if (err && err.status == 404) {
+        return dispatch({
+          type: types.AUTH_ERROR,
+          error: "Users doc not found in DB"
+        })
+      }
+      let foundUser = users[user.username]
+      if (! foundUser ) {
+        return dispatch({
+          type: types.AUTH_ERROR,
+          error: `User '${user.username}' Not Found`
+        })
+      }
+      let loggedIn = bcrypt.compareSync(user.password, foundUser.hashed_password) ? user.username : false
+      if (! loggedIn ) {
+        return dispatch({
+          type: types.AUTH_ERROR,
+          error: `Incorrect password for '${user.username}'`
+        })
+      }
+      dispatch({
+        type: types.AUTH_LOGGED_IN,
+        loggedIn: loggedIn
       })
-    }
-    let loggedIn = bcrypt.compareSync(user.password, foundUser.hashed_password) ? user.username : false
-    if (! loggedIn ) {
-      return dispatch({
-        type: types.AUTH_ERROR,
-        error: `Incorrect password for '${user.username}'`
-      })
-    }
-    dispatch({
-      type: types.AUTH_LOGGED_IN,
-      loggedIn: loggedIn
+      callback(loggedIn)
     })
-    callback(loggedIn)
   }
 }
 
