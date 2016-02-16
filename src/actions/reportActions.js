f96ad9e303144383755ecc84b3e2a147c45192be
David Rose <david@sxsw.com>
2016-02-15 19:26:48 -0500

    remove query in reportActions in favor of db view

--- src/actions/reportActions.js
+++ src/actions/reportActions.js
@@ -2,16 +2,16 @@ import * as types from '../constants/reportConstants'
 import database from '../database'
 
 export function jumpsByMonth() {
-  const query = {
-    map: function(doc) {
-      if(doc.type === 'student' && doc.jumps) {
-        for(var i in doc.jumps) {
-          emit(doc.jumps[i].jump_date.split(/T/)[0].split(/-/), 1)
-        }
-      }
-    },
-    reduce: "_count"
-  }
+  // const query = {
+  //   map: function(doc) {
+  //     if(doc.type === 'student' && doc.jumps) {
+  //       for(var i in doc.jumps) {
+  //         emit(doc.jumps[i].jump_date.split(/T/)[0].split(/-/), 1)
+  //       }
+  //     }
+  //   },
+  //   reduce: "_count"
+  // }
   return dispatch => {
     dispatch({ type: types.REPORT_REQUEST_DATA })
     database.query('students/jumps_by_month', {group_level: 2}, (err, response) => {
